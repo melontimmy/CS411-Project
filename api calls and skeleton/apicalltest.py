a2aa2debb7ada37987bae98f43a1c5136dcf7a1b
melontimmy <112988308+melontimmy@users.noreply.github.com>
2023-04-06 23:37:29 -0400

    Update apicalltest.py

--- api calls and skeleton/apicalltest.py
+++ api calls and skeleton/apicalltest.py
@@ -5,5 +5,13 @@ url = "http://www.themealdb.com/api/json/v1/1/filter.php?i=chicken"
 
 response = requests.request("GET", url)
 
-print(response.text)
+#print(response.text)
 
+
+url = "https://api.spoonacular.com/food/products/search?query=yogurt&apiKey="
+
+apikey = "a8fbdd86a4e74f5897ab358e550cc549"
+
+response2 = requests.request("GET", url+apikey)
+
+print(response2.text)
